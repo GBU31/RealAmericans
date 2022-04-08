96460851a5b2a64576e7429169839afd7d9bbc74
brookehorizon <danieltashman17@gmail.com>
2022-04-08 16:34:06 +0300

    Added get_post view

--- main/views.py
+++ main/views.py
@@ -7,6 +7,15 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 
 
+@login_required(login_url='/login')
+def get_post(request, pk):
+    get_post = post.objects.filter(pk=pk)
+    comments = comment.objects.filter(to_pk=pk)
+    all_ver = ver.objects.all()
+    pic = profilePic.objects.all()
+    return render(request, 'post.html', {'post':get_post, 'comments':comments, 'pic':pic,'all_ver':all_ver, 'User':str(request.user), })
+
+
 def Logout(request):
     logout(request)
     return redirect('login') 
@@ -29,7 +38,7 @@ def Login(request):
             login(request, user)
             return redirect('home')
         else:
-            return render(request, 'login.html', {'msg':'Error'})
+            return render(request, 'login.html', {'msg':['your username or password is incorrect']})
     return render(request, 'login.html')
 
 
@@ -51,7 +60,7 @@ def home(request):
 
 @login_required(login_url='/login')
 def profile(request):
-    all_post = post.objects.filter(user=str(request.user))
+    get_post = post.objects.filter(user=str(request.user))
     all_ver = ver.objects.all()
     pic = profilePic.objects.filter(user=str(request.user))
-    return render(request, 'profile.html', {'all_post':all_post,'User':str(request.user), 'pic':pic,'all_ver':all_ver})
+    return render(request, 'profile.html', {'get_post':get_post,'User':str(request.user), 'pic':pic,'all_ver':all_ver})
