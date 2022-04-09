885da4d3af25e2b4356755d011d6e308a2a73ae0
brookehorizon <danieltashman17@gmail.com>
2022-04-09 15:58:42 +0300

    added notifications view

--- main/views.py
+++ main/views.py
@@ -8,6 +8,20 @@ from django.contrib.auth.decorators import login_required
 
 
 @login_required(login_url='/login')
+def notifications(request):
+    notificationsmMention.objects.all().delete()
+    def find(user,all):
+        for i in all:
+            print(i)
+            if f'@{user}' in str(i):
+                
+                notificationsmMention.objects.create(myuser=request.user, user=i.user, to_pk=i.pk)
+    
+    find(request.user, post.objects.all())
+    find(request.user, comment.objects.all())
+    return render(request, 'notifications.html', {'mentions':notificationsmMention.objects.filter(myuser=str(request.user))})
+
+@login_required(login_url='/login')
 def get_post(request, pk):
     form = CommentForm()
     if request.method == 'POST':
