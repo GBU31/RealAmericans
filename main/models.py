5c42cf3ba44578ad12431dbc9d7e417f604564a9
brookehorizon <danieltashman17@gmail.com>
2022-04-10 18:01:47 +0300

    updated models.py

--- main/models.py
+++ main/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class profilePic(models.Model):
     user = models.CharField(max_length=100)
-    pic = models.CharField(max_length=500)
+    pic = models.CharField(max_length=100)
     def __str__(self):
         return self.user
 
