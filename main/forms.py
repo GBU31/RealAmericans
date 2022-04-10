420e8690575c4e9eb0148c486187c773364d1524
brookehorizon <danieltashman17@gmail.com>
2022-04-10 12:31:02 +0300

    Updated

--- main/forms.py
+++ main/forms.py
@@ -1,11 +1,17 @@
 from django import forms
 from .models import *
 
+class pfpForm(forms.ModelForm):
+    class Meta:
+        model = profilePic
+        fields = '__all__'
+
+
 
 class PostForm(forms.ModelForm):
     class Meta:
         model = post
-        fields = '__all__'
+        fields = ['Content']
         widgets = {
             'Content': forms.Textarea(attrs={'class':'post', 'placeholder':"what's new?", "style":"height:100px;"}),
         }
@@ -16,7 +22,7 @@ class PostForm(forms.ModelForm):
 class CommentForm(forms.ModelForm):
     class Meta:
         model = comment
-        fields = '__all__'
+        fields = ['Comment']
         widgets = {
             'Comment': forms.Textarea(attrs={'class':'post', "style":"height:100px;"}),
         }
--- static/js/comment.js
+++
@@ -1,6 +0,0 @@
-function RemoveInput() {
-    document.getElementById('id_user').style.display = 'none';
-    document.getElementById('id_to_pk').style.display = 'none';
-}
-
-RemoveInput()
