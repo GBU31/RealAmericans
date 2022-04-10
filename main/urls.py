97a1e0a86aec338c5885abd9a95ba5b618828a04
brookehorizon <danieltashman17@gmail.com>
2022-04-10 18:00:29 +0300

    added pfp path

--- main/urls.py
+++ main/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('signup', views.signup, name='signup'), 
     path('post/<int:pk>', views.get_post, name='get_post'),
     path('logout', views.Logout),
-    path('notifications', views.notifications, name='notifications')
+    path('notifications', views.notifications, name='notifications'),
+    path('pfp', views.add_pfp)
 ]
