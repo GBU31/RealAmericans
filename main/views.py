d3a3dd279da3a611936eb25e9a6d827b3b5f4adc
brookehorizon <danieltashman17@gmail.com>
2022-04-10 17:59:52 +0300

    updated add_pfp view

--- main/views.py
+++ main/views.py
@@ -6,15 +6,23 @@ from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 
+@login_required(login_url='/login')
+def add_pfp(request):
+    form = pfpForm()
+    if request.method == 'POST':
+        form = pfpForm(request.POST)
+        if form.is_valid:
+            profilePic.objects.filter(user=str(request.user)).delete()
+            form2 = form.save(commit=False)
+            form2.user = request.user
+            form2.save()
+            return redirect('profile')
 
-
-
+    return render(request, 'add_pfp.html', {'form':form})
 
 @login_required(login_url='/login')
 def notifications(request):
-    
     notificationsmMention.objects.all().delete()
-
     def findPost(user,all):
         for i in all:
             if f'@{user}' in str(i):
@@ -46,7 +54,6 @@ def get_post(request, pk):
     pic = profilePic.objects.all()
     return render(request, 'post.html', {'post':get_post, 'form':form,'comments':comments, 'pic':pic,'all_ver':all_ver, 'User':str(request.user), })
 
-
 def Logout(request):
     logout(request)
     return redirect('login') 
@@ -72,7 +79,6 @@ def Login(request):
             return render(request, 'login.html', {'msg':['your username or password is incorrect']})
     return render(request, 'login.html')
 
-
 @login_required(login_url='/login')
 def home(request):
     all_post = post.objects.all()
@@ -87,7 +93,6 @@ def home(request):
             form2.save()
 
     return render(request, 'home.html', {'mentions':notificationsmMention.objects.filter(myuser=str(request.user)), 'post_form':form,'all_post':all_post, 'User':str(request.user), 'pic':pic,'all_ver':all_ver})
-
 
 @login_required(login_url='/login')
 def profile(request):
